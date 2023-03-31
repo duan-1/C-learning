7f65966a0030bbf0b0c0afdc4a7953da940eaee3
patoria <patoria11@gmail.com>
2023-03-31 09:30:46 +0800

    ✨ feat: added example code

---
+++ Chapter 4/pizza.c
@@ -0,0 +1,18 @@
+/* pizza.c -- 在比萨饼程序中使用已定义的常量 */
+#include <stdio.h>
+
+#define PI 3.14159
+
+int main(void)
+{
+    float area, circum, radius;
+
+    printf("What is the radius of your pizza?\n");
+    scanf("%f", &radius);
+    area = PI * radius * radius;
+    circum = 2.0 * PI * radius;
+    printf("Your basic pizza parameters are as follows:\n");
+    printf("circumference = %.2f, area = %.2f\n", circum, area);
+
+    return 0;
+}
