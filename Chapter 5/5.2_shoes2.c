7644082f8495ae5bc9776993848486e27c4f3ec7
patoria <patoria11@gmail.com>
2023-04-26 15:58:40 +0800

    ✨ feat: added example code

--- Chapter 5/5.2_shoes2.c
+++ Chapter 5/5.2_shoes2.c
@@ -8,12 +8,11 @@ int main(void)
     const double SCALE = 0.333; //const 变量
     double shoe, foot;
     printf("Shoe size (men's) foot length\n");
-    shoe = 3.0;
-    while (shoe < 18.5) //while 循环开始
+    shoe = 2.0;
+    while (++shoe < 18.5) //while 循环开始
     {   //块开始
         foot = SCALE * shoe + ADJUST;
         printf("%10.1f %15.2f inches\n", shoe, foot);
-        shoe = shoe + 1.0;
     }   //块结束
     printf("If the shoe fits, wear it\n");
 
---
+++ Chapter 5/5.9_min_sec.c
@@ -0,0 +1,27 @@
+/* min_sec.c -- 把秒数转换成分和秒 */
+#include <stdio.h>
+
+#define SEC_PER_MIN 60  //1 分钟 60 秒
+
+int main(void)
+{
+    int sec, min, left;
+    printf("Convert seconds to minutes and seconds!\n");
+    printf("Enter the number of seconds (<=0 to quit):\n");
+    fflush(stdin);
+    scanf("%d", &sec);  //读取秒数
+    while (sec > 0)
+    {
+        min = sec / SEC_PER_MIN;    //阶段分钟数
+        left = sec % SEC_PER_MIN;   //剩下的秒数
+        printf("%d seconds is %d minutes, %d seconds.\n", sec, min, left);
+        printf("Enter next value (<=0 to quit):\n");
+        fflush(stdin);
+        scanf("%d", &sec);
+    }
+    printf("Done!\n");
+
+    fflush(stdin);
+    getchar();
+    return 0;
+}
